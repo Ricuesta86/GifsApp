66594427655179f326a679788f6277c880fee317
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-02-01 00:08:13 -0600

    Se creo la lista de Categorias

--- src/Componets/GifExpertApp.js
+++ src/Componets/GifExpertApp.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Push', 'Dragon Ball','Bleach'];
+    const [categories, setcategories] = useState(['One Push', 'Dragon Ball','Bleach']);
+
+    const handleAdd =() => {
+        // setcategories(
+        //     [...categories,'Black clover']
+        // )
+        setcategories(cats=>[...cats,'Black clover'])
+    }
+
     return (
         <>  
             <h2>GifExpertApp</h2>
             <hr/>
+            <button  onClick= { handleAdd }>Addicionar</button>
+            <ol>
+            {
+                categories.map( category => {
+                    return <li key={category}>{category}</li>
+                })
+            }
+            </ol>
+
         </>
     )
 }
 
-export default GifExpertApp
+export default GifExpertApp;
  
