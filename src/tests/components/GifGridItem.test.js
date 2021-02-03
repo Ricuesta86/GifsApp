49f7c4056022080e55f443d773d7ad34230a3fd5
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-02-03 01:35:56 -0600

    Actualizando el snapshot y creando los proptypes de GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,10 +3,19 @@ import { shallow } from 'enzyme';
 import GifGridItem from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
+    
+    const title = 'Hola mundo';
+    const url = 'https://localhost/algo.jpg';
+
 
     test('debe mostrar el componente correctamente ', () => {
 
-        const wrapper = shallow( <GifGridItem /> );
+
+
+        const wrapper = shallow( <GifGridItem 
+                title={ title }
+                url = { url }
+                /> );
         expect( wrapper ).toMatchSnapshot();
     })
     
