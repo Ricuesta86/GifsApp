6ea7cc4ced2c665ecedaf8487ce5f11757dc93b3
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-02-06 07:53:17 -0500

    Pruebas del componente GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -7,17 +7,44 @@ describe('Pruebas en <GifGridItem />', () => {
     const title = 'Hola mundo';
     const url = 'https://localhost/algo.jpg';
 
+    const wrapper = shallow( <GifGridItem 
+            title={ title }
+            url = { url }
+            /> );
 
     test('debe mostrar el componente correctamente ', () => {
 
 
 
-        const wrapper = shallow( <GifGridItem 
-                title={ title }
-                url = { url }
-                /> );
         expect( wrapper ).toMatchSnapshot();
     })
+    
+    test('debe tener un párrafo con el title', () => {
+        
+        const p = wrapper.find( 'p' );
+        expect( p.text().trim() ).toBe( title );
+
+    })
+    
+    test('debe tener la imagen igual a la url y el alt al title', () => {
+        
+        const img = wrapper.find('img');
+        // console.log(img.props());
+
+        expect( img.props().src.trim() ).toBe(url);
+        expect( img.props().alt.trim() ).toBe(title);
+
+    })
+
+    test('debe tener la clase animate__fadeInUp', () => {
+        
+        const div = wrapper.find('div');
+        // console.log( div.prop('className') );
+        const className = div.prop('className');
+
+        expect(className.includes('animate__fadeInUp')).toBe(true);
+    })
+    
     
     
     
