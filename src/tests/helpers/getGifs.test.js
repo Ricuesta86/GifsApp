84eadd022b8591646be838ce9a78f6e23f3e0d56
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-02-07 23:54:55 -0500

    Prueba al helper getGifs

---
+++ src/tests/helpers/getGifs.test.js
@@ -0,0 +1,23 @@
+import { getGifs } from "../../helpers/getGifs"
+
+describe('Prueba con getGifs fecth', () => {
+
+    test('debe traer 10 elementos ', async () => {
+        
+        const gifs = await getGifs( 'Dragon Ball' );
+
+        expect(gifs.length).toBe(10);
+    })
+    
+
+    test('debe traer arreglo vacio ', async () => {
+        
+        const gifs = await getGifs( '' );
+
+        expect(gifs.length).toBe(0);
+    })
+
+
+
+    
+})
